d55e6faa1eb3213713cf04989470ab3601c472e6
Amelie Thouvenot <amelie.thouvenot@kellogg.northwestern.edu>
2024-01-11 03:21:24 +0000

    final changes prompt 41

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -2,7 +2,7 @@
 
 SELECT SUM(stats.hits)
 FROM players 
-INNER JOIN stats ON stats.player_id = players.id
+INNER JOIN stats ON stats.player_id = players.id -- order here doesn't matter, as long as the keys correspond to each other
 WHERE players.first_name = "Barry" 
 AND players.last_name = "Bonds"
 ; 
